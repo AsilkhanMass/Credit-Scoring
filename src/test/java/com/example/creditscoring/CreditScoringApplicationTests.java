30da4582e6688dafdf89b5852bdfe938c7009964
Asilkhan Abdumaminov <asilkhan566766@gmail.com>
2024-09-10 14:22:26 +0500

    Initial commit

---
+++ src/test/java/com/example/creditscoring/CreditScoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.creditscoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditScoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
